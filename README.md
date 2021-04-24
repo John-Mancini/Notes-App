42d16517b2054d1f1908d41b31de2d6cc558674a
John-Mancini <mancinijohn5988@gmail.com>
2021-04-24 14:53:01 -0400

    Connected the delete function to server.js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Notes-App
+
+THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+WHEN I click on an existing note in the list in the left-hand column
+THEN that note appears in the right-hand column
--- server.js
+++ server.js
@@ -10,12 +10,37 @@ const PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
+let data = [
+  {
+    title: "a",
+    id: 0,
+    text: "b",
+  },
+  {
+    title: "c",
+    id: 1,
+    text: "d",
+  },
+];
 // Routes
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", (req, res) => res.sendFile((__dirname, "view.html")));
+app.get("/", (req, res) => res.sendFile(__dirname + "/public/index.html"));
 
+app.get("/notes", (req, res) => res.sendFile(__dirname + "/public/notes.html"));
+
+app.delete("/api/notes/:id", (req, res) => {
+  console.log("params", req.params);
+  res.json([{ title: "title", text: "text" }]);
+});
+
+app.get("/api/notes", (req, res) => res.json(data));
+
+app.post("/api/notes", (req, res) => {
+  console.log("body", req.body);
+});
 // Starts the server to begin listening
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
