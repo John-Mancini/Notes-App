59f7398a2462937c41fdb0887849c43b70c61899
John-Mancini <mancinijohn5988@gmail.com>
2021-04-28 18:01:55 -0400

    made post and delete functional cleaned up log.js

--- db/log.js
+++ db/log.js
@@ -3,12 +3,17 @@ const fs = require("fs");
 // Package used to generate unique ids from "https://www.npmjs.com/package/uniqid"
 const uniqid = require("uniqid");
 
+//
 const util = require("util");
 
 //convert a method that returns responses using a callback function to return responses in a promise object
-const readFile = util.promisify(fs.readFile);
+// const readFile = util.promisify(fs.readFile);
 
-const writeFile = util.promisify(fs.writeFile);
+// const writeFile = util.promisify(fs.writeFile);
+
+const readFile = fs.readFileSync;
+
+const writeFile = fs.writeFileSync;
 
 class Log {
   //reading the file from the db.json using utf8 encoding
@@ -17,22 +22,33 @@ class Log {
   }
   // writing the file to db.json using JSON.stringify
   write(notes) {
-    return writeFile("/db/db.json", JSON.stringify(notes));
+    return writeFile("./db.json", JSON.stringify(notes));
   }
 
   readNotes() {
-    return this.read().then((notes) => {
-      let notesArr;
+    //  try {
+    const data = this.read();
+    console.log(data);
+    const notesArr = JSON.parse(data);
+    console.log(notesArr);
 
-      // try to take the notes and combine them into an array and if it doesnt work then return an empty array
+    return notesArr;
+    // } catch (err) {
+    return [];
+    // }
 
-      try {
-        notesArr = [].concat(JSON.parse(notes));
-      } catch (err) {
-        notesArr = [];
-      }
-      return notesArr;
-    });
+    //return this.read().then((notes) => {
+    //  let notesArr;
+
+    // try to take the notes and combine them into an array and if it doesnt work then return an empty array
+
+    //   try {
+    //     notesArr = [].concat(JSON.parse(notes));
+    //   } catch (err) {
+    //     notesArr = [];
+    //   }
+    //   return notesArr;
+    // });
   }
 }
 module.exports = new Log();
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 //dependencies
 
 const express = require("express");
-
+const fs = require("fs");
+const uniqid = require("uniqid");
 // Sets up the Express App
 
 const app = express();
 const PORT = 3000;
-const Log = require("./db/log");
+const Log = require("./db/log.js");
+console.log(Log);
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -34,17 +36,37 @@ app.get("/notes", (req, res) => res.sendFile(__dirname + "/public/notes.html"));
 
 app.delete("/api/notes/:id", (req, res) => {
   console.log("params", req.params);
+  let data = fs.readFileSync("./db/db.json", "utf8");
+  let notesArr = JSON.parse(data);
+  notesArr = notesArr.filter((item) => {
+    return item.id !== req.params.id;
+  });
+  const jsonString = JSON.stringify(notesArr);
+  fs.writeFileSync("./db/db.json", jsonString);
+  res.json(notesArr);
   // use the params id to find the item in my data and remove it.
-  res.json([{ title: "title", text: "text" }]);
+  //res.json([{ title: "title", text: "text" }]);
 });
 
 app.get("/api/notes", (req, res) => {
-  Log.readNotes().then((notes) => res.json(notes));
+  // const notes = Log.readNotes();
+  let data = fs.readFileSync("./db/db.json", "utf8");
+  const notesArr = JSON.parse(data);
+  res.json(notesArr);
+  //Log.readNotes().then((notes) => res.json(notes));
 });
 
 app.post("/api/notes", (req, res) => {
   console.log("body", req.body);
+  let data = fs.readFileSync("./db/db.json", "utf8");
+  const notesArr = JSON.parse(data);
+  req.body.id = uniqid();
+  notesArr.push(req.body);
+  const jsonString = JSON.stringify(notesArr);
+  fs.writeFileSync("./db/db.json", jsonString);
+  res.json(notesArr);
 });
+
 // Starts the server to begin listening
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
